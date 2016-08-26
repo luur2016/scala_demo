5808d2df9ced5e8c8b1c85254cf753cc14ce4fec
wangkai <wangkai@zy.com>
2016-08-27 06:31:47 +0800

    add test file

--- src/FirstClass.scala
+++ src/FirstClass.scala
@@ -1,25 +1,9 @@
 import scala.beans.BeanProperty
 import MyModule.abs
 
-class FirstClass {
-
-  private var name="";
-  private var age = 0;
-  
-  def this(name:String){
-    this()
-    this.name = name
-  }
-  
-  def this(name:String,age:Int){
-    this()
-    this.name = name
-    this.age = age
-  }
-  
+object FirstClass {
   def main(args: Array[String]): Unit = {
-//    val f = new FirstClass("haha",27)
-//    println(f.name)
-    println(abs(-30))
-  } 
+    val xs:List[Int] = List(1,2,3,4,5)
+    val ex1 = List.dropWhile(xs, (x:Int) => x<4)
+  }
 }
--- src/List.scala
+++ src/List.scala
@@ -13,4 +13,45 @@ object List {
     case Cons(0.0,_) => 0.0
     case Cons(x,xs) => x * product(xs)
   }
+  
+  def apply[A](as:A*):List[A]={
+    if(as.isEmpty) Nil
+    else Cons(as.head,apply(as.tail:_*))
+  }
+  
+  def tail[A](ds:List[A]):List[A] =ds match {
+    case Nil =>sys.error("tail of empty list")
+    case Cons(_,t)=>t
+  }
+  
+  def setHead[A](h:A,ds:List[A]):List[A] =ds match{
+    case Nil =>sys.error("tail of empty list")
+    case Cons(_,t) => Cons(h,t)
+  }
+
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_, t) => drop(t, n - 1)
+   }
+  
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Cons(h,t) if f(h) => dropWhile(t, f)
+      case _ => l
+    }
+
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Nil => sys.error("init of empty list")
+      case Cons(_, Nil) => Nil
+      case Cons(h, t) => Cons(h, init(t))
+    }
+  
+  def foldRight[A,B](as:List[A],z:B)(f:(A,B)=>B):B=
+    as match{
+      case Nil => z
+      case Cons(x,xs)=>f(x,foldRight(xs,z)(f))
+    }
 }
